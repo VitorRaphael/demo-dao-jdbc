815e0ac64bd9a55d076f53072f2d8c8ccb785de4
Vitor Raphael Gil <vitorraphaelbank@gmail.com>
2026-06-18 20:26:15 -0300

    delete implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -9,7 +9,7 @@ public interface SellerDao {
 
     void insert (Seller obj);
     void update (Seller obj);
-    void deleteById(Seller id);
+    void deleteById(Integer id);
     Seller findById (Integer id); // Pega esse id e consulta no banco de dados um objeto com esse id
 
     // Vai retornar todos os departamentos
