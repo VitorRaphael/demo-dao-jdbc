c2e57901478ff39e7b4f5a3d87d1e9b76c9848d9
Vitor Raphael Gil <vitorraphaelbank@gmail.com>
2026-06-18 20:40:19 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.dao.ipml.DepartmentDaoJDBC;
 import model.dao.ipml.SellerDaoJDBC;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
