fee6828b9ff83be533af5f3a7cbba88db7169447
Vitor Raphael Gil <vitorraphaelbank@gmail.com>
2026-06-18 13:09:37 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 public class Program {
@@ -11,6 +13,8 @@ public class Program {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, obj, 3000.0, "bob@gmail.com", new Date(), "Bob");
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
